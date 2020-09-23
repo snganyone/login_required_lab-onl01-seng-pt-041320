4c9beca156f26f3cc304c5a1ce6949c4ddb2e6d5
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-22 21:11:26 -0700

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,2 +1,12 @@
 class SecretsController < ApplicationController
+    before_action :require_login
+
+    def show
+    end
+
+    private
+
+    def require_login
+        return head(:forbidden) unless session.include? :user_id
+    end
 end
